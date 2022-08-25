ce4bc179556f722fd02d2f72b0e84de840db8e02
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-08-25 21:05:20 +0600

    testing with object destructring

---
+++ test.js
@@ -0,0 +1,11 @@
+const fish = {
+    name: "elish",
+    age: 6,
+    phone: 1545152,
+    address: "Chandpur"
+}
+
+let { name, age, phone, address } = fish;
+
+console.log(name);
+console.log(address);
